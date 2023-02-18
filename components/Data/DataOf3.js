38c3324e7b24c10dccee00c10ab0a59bfbd426ba
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-02-18 20:15:06 +0530

    Completed with md breakpoint responsiveness of DataOf3.js

--- components/Data/DataOf3.js
+++ components/Data/DataOf3.js
@@ -3,11 +3,11 @@ import React from 'react'
 const DataOf3 = ({ Heading, Description, Code1, Code2, Code3, Code1Desc, Code2Desc, Code3Desc }) => {
 
     return (
-        <div className="py-1 text-white ">
+        <div className="py-1 text-white md:w-10/12 lg:w-3/4">
             {/* Top Section */}
             <div className="flex flex-col justify-center items-center">
                 {/* Header  */}
-                <h1 className="text-white text-center text-[35px] font-black">
+                <h1 className="text-white text-center text-[35px] font-black md:text-[40px] lg:text-[50px]">
                     {Heading}
                 </h1>
                 {/* Header Description */}
@@ -17,36 +17,36 @@ const DataOf3 = ({ Heading, Description, Code1, Code2, Code3, Code1Desc, Code2De
             </div>
 
             {/* Command Section  */}
-            <div className="mt-1 ">
-                <div className="p-2 my-3 bg-[#424242] rounded-lg flex flex-col justify-center items-center">
+            <div className="mt-1">
+                <div className="p-2 my-3 bg-[#424242] rounded-lg flex flex-col justify-center items-center lg:p-5">
                     {/* Command */}
-                    <p className="bg-[#D9D9D9] text-black px-5 py-3 rounded-lg font-firaCode text-[13.65px] font-black cursor-pointer">
+                    <p className="bg-[#D9D9D9] text-black text-center px-5 py-3 rounded-lg font-firaCode text-[13.65px] font-black cursor-pointer lg:text-[16px]">
                         {Code1}
                     </p>
                     {/* Command Description  */}
-                    <p className="mt-1 text-center">
+                    <p className="mt-1 text-center lg:text-[16px]">
                         {Code1Desc}
                     </p>
                 </div>
 
                 <div className="p-2 my-3 bg-[#424242] rounded-lg flex flex-col justify-center items-center">
                     {/* Command */}
-                    <p className="bg-[#D9D9D9] text-black px-5 py-3 rounded-lg font-firaCode text-[13.65px] font-black cursor-pointer">
+                    <p className="bg-[#D9D9D9] text-black px-5 py-3 rounded-lg font-firaCode text-[13.65px] font-black cursor-pointer lg:text-[16px]">
                         {Code2}
                     </p>
                     {/* Command Description  */}
-                    <p className="mt-1 text-center">
+                    <p className="mt-1 text-center lg:text-[16px]">
                         {Code2Desc}
                     </p>
                 </div>
 
                 <div className="p-2 my-3 bg-[#424242] rounded-lg flex flex-col justify-center items-center">
                     {/* Command */}
-                    <p className="bg-[#D9D9D9] text-black px-5 py-3 rounded-lg font-firaCode text-[13.65px] font-black cursor-pointer">
+                    <p className="bg-[#D9D9D9] text-black px-5 py-3 rounded-lg font-firaCode text-[13.65px] font-black cursor-pointer lg:text-[16px]">
                         {Code3}
                     </p>
                     {/* Command Description  */}
-                    <p className="mt-1 text-center">
+                    <p className="mt-1 text-center lg:text-[16px]">
                         {Code3Desc}
                     </p>
                 </div>
